096e45954b50a71bac780c767e3bd16d4adf9327
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-06-28 19:34:45 +0530

    Update swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,1 +1,33 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const options = {
+    definition: {
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [{ bearerAuth: [] }],
+
+        openapi: '3.0.0',
+        info: {
+            title: 'Drone API',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // All route files will be scanned
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
